5ca9d82a5730dc7e80122eade5b44823df8c360a
Gerson Sec <gsecg1@miumg.edu.gt>
2023-06-04 04:00:25 -0600

    Corregí el problema con Serializable

---
+++ api_candidatos/src/main/java/com/proyectofinal/api_candidatos/modelos/Cliente.java
@@ -0,0 +1,65 @@
+package com.proyectofinal.api_candidatos.modelos;
+
+public class Cliente implements Serializable{
+    private static final long serialVeresionUID = 1l;
+    
+    private long id;
+    
+    private String nombre;
+
+    private String apellido;
+
+    private String email;
+
+    public Cliente()
+
+
+    {
+
+    public st getAtic() {
+        return atic;
+    }
+
+    public void setAtic(st atic) {
+        this.atic = atic;
+    }
+
+    public long getSerialVeresionUID() {
+        return serialVeresionUID;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    
+        
+    }
+
+        
+    
+}
